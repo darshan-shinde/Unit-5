cd43cd2fd08f66e905c64fe14177c2f0bf270964
shindedarshan502 <96101838+shindedarshan502@users.noreply.github.com>
2022-04-08 00:35:07 +0530

    y

--- assignment2/src/App.js
+++ assignment2/src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
  const systems = ["Android","Blackberry","iPhone","Windows Phone"];
  const opr = ["Samsung","HTC","Micromax","Apple"]
+ const middle = ["Sevices","Projects","About"]
  return(
    <div className="App">
-    <div>
+     <div id="nav">
+          <p>LOGOBAKERY</p>
+        <div class="middle">
+          {middle.map((e) => {
+          return <p>{e}</p>
+          })}
+        </div>
+        <button>Contact</button>
+     </div>
+  <div>
    <h1>Mobile Operating System</h1> 
     <ul>
       {systems.map((e) => {
@@ -27,7 +37,5 @@ function App() {
  )
 
 }
-function Mobiles(){
 
-}
 export default App;
