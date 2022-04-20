3f274149a35a4547ac5c86c500286230a3750329
shindedarshan502 <96101838+shindedarshan502@users.noreply.github.com>
2022-04-20 20:36:46 +0530

    y

---
+++ assignment6/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+import {Button} from './components/button'
+
+function App() {
+  return (
+    <div className="App">
+      <Button>Primary Button</Button>
+      <Button>Default Button</Button>
+      <Button>Dashed Button</Button>
+      <Button>Text Button</Button>
+      <Button>Link Button</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ assignment6/src/components/button.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+
+const Button = styled.button`
+  margin : 10px;
+  border : none;
+  background-color:skyblue;
+  color:white;
+  padding:10px;
+  border-radius:7px;
+&:hover{
+    color:black;
+}
+ 
+
+
+`;
+
+export {Button}
