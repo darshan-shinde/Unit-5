6c809d969a8efa24144be27c3619061bd57d5dfb
shindedarshan502 <96101838+shindedarshan502@users.noreply.github.com>
2022-04-23 15:18:57 +0530

    y

--- router-project/src/components/navbar.jsx
+++ router-project/src/components/navbar.jsx
@@ -1,5 +1,9 @@
+import { Route,Routes } from "react-router-dom"
+
+// import {home} from "./components/home"
+// import {Products} from "./components/Products"
 const Navbar = () => {
-  
+    // const [nav,setnav] = 
     return <div><h1>Darshan Shinde</h1></div>
 }
 
