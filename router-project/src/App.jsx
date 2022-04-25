97bf7a84c571e4673b4a6e43dd55936321a1fb5f
shindedarshan502 <96101838+shindedarshan502@users.noreply.github.com>
2022-04-25 13:59:28 +0530

    y

--- router-project/src/App.jsx
+++ router-project/src/App.jsx
@@ -1,11 +1,19 @@
 
 import './App.css';
+import { Routes, Route} from "react-router-dom"
 import {Navbar} from './components/navbar'
+import {Home} from "./components/home"
+import {Products} from "./components/Products"
+
 
 function App() {
   return (
     <div className="App">
-     <Navbar></Navbar>
+     <Navbar />
+     <Routes>
+       <Route path="/" element={<Home />}></Route>
+       <Route path="/Products" element={<Products />}></Route>
+     </Routes>
     </div>
   );
 }
--- router-project/src/components/navbar.jsx
+++ router-project/src/components/navbar.jsx
@@ -1,10 +1,22 @@
-import { Route,Routes } from "react-router-dom"
+import {Link} from "react-router-dom";
+import "./all.css";
 
-// import {home} from "./components/home"
-// import {Products} from "./components/Products"
 const Navbar = () => {
-    // const [nav,setnav] = 
-    return <div><h1>Darshan Shinde</h1></div>
+    const nav = [
+        {title:"home" , to:"/"},
+        {title:"Products" , to:"/Products"},
+        {title:"Cart" , to:"/Cart"},
+    ];
+    return (
+       <div className="main">
+           <div className="left">Darshan Shinde</div>
+          <div className="right">
+            {nav.map(e => 
+               <Link key={e.title} to={e.to} style={{margin:"5px" , TextDecoration: "none"}}>{e.title}</Link>   
+              )} 
+           </div> 
+       </div>
+        );
 }
 
 export {Navbar}
